7a9ed5a2412923929e09351c07f78963c21f1af7
Claire <claire0505@gmail.com>
2018-09-07 15:26:06 +0800

    initial commit LoginActivity Login design

---
+++ app/src/main/java/claire/com/MainActivity.java
@@ -0,0 +1,19 @@
+package claire.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    boolean logon = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!logon) //如果末登入，則開啟LoginActivity
+        {
+
+        }
+    }
+}
