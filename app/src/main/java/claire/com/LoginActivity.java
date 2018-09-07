06808fb334242356fef5a65a06ea22157817e0f5
Claire <claire0505@gmail.com>
2018-09-07 16:21:38 +0800

    轉換Activity取得結果 startActivityResult() onActivityResult()

--- app/src/main/java/claire/com/LoginActivity.java
+++ app/src/main/java/claire/com/LoginActivity.java
@@ -23,6 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         //測式帳密 jack/1234
         if (uid.equals("jack") && pw.equals("1234")){
             Toast.makeText(this, "登入成功", Toast.LENGTH_SHORT).show();
+
+            getIntent().putExtra("LOGIN_USERID", uid);
+            getIntent().putExtra("LOGIN_PASSWD", pw);
+            // 先將帳密放入Intent物件中，再呼叫Activity的setResult方法，設定這一個功能畫面的結果
+            // 為RESULT_OK，並把Intent物件放入結果中，目的在結束後，回去MainActivity後仍可得到結果
+            setResult(RESULT_OK, getIntent());
+
             finish(); //正確結束本活動，回到MainActivity畫面
 
         }else{
--- app/src/main/java/claire/com/MainActivity.java
+++ app/src/main/java/claire/com/MainActivity.java
@@ -3,8 +3,10 @@ package claire.com;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int RC_LOGIN = 1;
     boolean logon = false;
 
     @Override
@@ -15,8 +17,31 @@ public class MainActivity extends AppCompatActivity {
         if (!logon) //如果末登入，則開啟LoginActivity
         {
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
 
+            // startActivity(intent); // 使用這個方式，按返回鍵一樣可以不用登入回到主畫面
+
+            // 1.定義代表功能的常數
+            // 2.startActivityForResult 是到另一個畫面中並取得結果(資料)
+            // 3.在功能畫面結束之前設定返回值 (LoginActivity)
+            // 4.回到主功能onActivityResult方法 (LoginActivity)
+            startActivityForResult(intent, RC_LOGIN);
+
+        }
+    }
+
+    // LoginActivity 回到主功能，覆寫OnActivityResult方法
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_LOGIN){
+            //第二與第三個參數是由3.結束前所設定的RESULT_OK與Intent物件 (resultCode與data)
+            if (resultCode == RESULT_OK){
+                String uid = data.getStringExtra("LOGIN_USERID");
+                String pw = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT", uid + "/" + pw);
+            }
+        }else {
+            finish();
         }
     }
 }
