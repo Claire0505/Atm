e70a50c4d125e205b77dabbf7c4ba75df4bf4a2b
Claire <claire0505@gmail.com>
2018-09-11 22:44:15 +0800

    ListView與Spinner 使用ArrayAdapter

--- app/src/main/java/claire/com/MainActivity.java
+++ app/src/main/java/claire/com/MainActivity.java
@@ -6,16 +6,51 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final int RC_LOGIN = 1;
     boolean logon = false;
+    String[] func = {"餘額查詢","交易明細", "最新消息", "投資理財", "離開"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //清單 ListView
+        ListView list = findViewById(R.id.list);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_expandable_list_item_1, func);
+
+        list.setAdapter(adapter);
+
+        //使用Spinner
+        Spinner notify = findViewById(R.id.spinner);
+        //因為陣列是放在res專案資源中，可以使用ArrayAdapter的類別方法createFromResource，
+        //直接產生一個ArrayAdapter<CharSequence>的物件
+        final ArrayAdapter<CharSequence> nAdapter =
+                ArrayAdapter.createFromResource(this,
+                        R.array.notify_array,
+                        android.R.layout.simple_spinner_dropdown_item);
+        notify.setAdapter(nAdapter);
+        //Spinner項目選擇時的傾聽器
+        notify.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        nAdapter.getItem(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         // 測試TestActivity
         // startActivity(new Intent(this, TestActivity.class));
