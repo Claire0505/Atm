edc807707fd65c73db8b8043e4cf715b11cd763f
Claire <claire0505@gmail.com>
2018-09-24 22:07:43 +0800

    使用SimpleCursorAdapter ListView的資料來源若是查詢結果-[Cursor]物件時， 可使用SimpleCursorAdapter建立Adapter物件。

--- app/src/main/java/claire/com/FinanceActivity.java
+++ app/src/main/java/claire/com/FinanceActivity.java
@@ -1,12 +1,15 @@
 package claire.com;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
 
@@ -24,6 +27,20 @@ public class FinanceActivity extends AppCompatActivity {
                 startActivity(new Intent(FinanceActivity.this, AddActivity.class));
             }
         });
+
+        ListView list = findViewById(R.id.list);
+        MyDBHelper dbHelper = new MyDBHelper(this, "expense.db", null, 1);
+        Cursor c = dbHelper.getReadableDatabase().query(
+                "exp", null, null,null, null, null, null);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_expandable_list_item_2,  //傳入版面資源代表一列資料中顯示兩個TextView
+                c,  //傳入所查詢的Cursor物件
+                new String[]{"info","amount"}, //顯示[info][amount]兩個欄位
+                new int[] {android.R.id.text1, android.R.id.text2}, //兩個欄位所對應版面中的id值與flags參數
+                0); //flags如果給「0」代表ListView在展示過程中資料庫中的記錄如果更動了，
+                         // ListView將不自動重新查詢並更動畫面中的資料。
+        list.setAdapter(adapter);
     }
 
 }
