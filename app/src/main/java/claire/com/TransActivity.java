b99d2fbb4e3175623fa80aa0b5321bd057b444af
Claire <claire0505@gmail.com>
2018-10-07 21:26:43 +0800

    使用OkHttp第三方函式庫 OkHttp 的特色為： 1.支援HTTP/2 ，讓連到一個主機的多個要求都能共用一個socket通道2.使用連接池 (Connection Pool) 以降低產生要求時的延遲時間 3.使用 GZIP 壓縮技術降低下載的資料大小 4.回應的快取機制以避免多次連至同一網址時的負荷

--- app/src/main/java/claire/com/TransActivity.java
+++ app/src/main/java/claire/com/TransActivity.java
@@ -11,14 +11,47 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class TransActivity extends AppCompatActivity {
+    //使用OkHttp第三方函式庫
+    OkHttpClient client = new OkHttpClient();
+    private static final String AtmHistoryURL = "http://atm201605.appspot.com/h";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans);
 
-        new TransTask().execute("http://atm201605.appspot.com/h");
+        //OKHttp,使用Request.Builder設定一個連線必要的資訊，此時尚未連線至主機
+        Request request = new Request.Builder()
+                .url(AtmHistoryURL)
+                .build();
+        //使用OkHttpClient的newCall方法建立一個呼叫物件，此時尚未連線至主機
+        Call call = client.newCall(request);
+        //呼叫Call類別的enqueue方法，進行排程連線，此時才實際連至主機
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //告知使用者連線失數
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //取得伺服器的回應字串
+                String json = response.body().string();
+                Log.d("OKHTTP", json);
+                //解析JSON
+                parseJSON(json);
+
+            }
+        });
+
+        //new TransTask().execute("http://atm201605.appspot.com/h");
     }
 
     //使用[AsyncTask]類別設計HTTP連線工作
